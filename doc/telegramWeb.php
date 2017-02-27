299e1e49511a450e3b7a98f8cf391bdce13e23fd
volidamar <volidamar@mail.ru>
2017-02-27 15:34:39 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -9,3 +9,4 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 file_get_contents("https://api.telegram.org/bot376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8/sendMessage?chat_id=".$id."&text=i can see you");
 file_put_contents("doc/logs.txt",$id);
+print_r($output);

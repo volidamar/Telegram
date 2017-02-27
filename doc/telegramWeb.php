7c79fb113e7f18fb82df1840626410801f941472
volidamar <volidamar@mail.ru>
2017-02-27 15:55:36 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -5,10 +5,6 @@
  * Date: 27.02.17
  * Time: 2:10 PM
  
- */$filePath='https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/getupdates;
-    $jsonData = file_get_contents($filePath, false, stream_context_create($arrContextOptions));
-        $json = json_decode($jsonData, true);
-        foreach($json['result] as $res{
-  $id=$res['message']['message_id']; 
- }
-file_get_contents("https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/sendMessage?chat_id=."$id".&text=pret Serii");
+ */
+
+file_get_contents("https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/sendMessage?chat_id=222985371&text=pret Serii");

3704a329abb565c10da460a7f569e7bfbde57051
volidamar <volidamar@mail.ru>
2017-02-27 15:55:49 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -4,7 +4,6 @@
  * User: mac
  * Date: 27.02.17
  * Time: 2:10 PM
- 
  */
 
 file_get_contents("https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/sendMessage?chat_id=222985371&text=pret Serii");

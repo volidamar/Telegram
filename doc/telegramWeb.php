6946048bff24dcfc88ed84701ca4eaeda77b910b
volidamar <volidamar@mail.ru>
2017-02-27 17:47:12 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -6,16 +6,3 @@
  * Time: 2:10 PM
  */
 
-$output = file_get_contents('php://input');
-file_put_contents("message.txt",$output,FILE_APPEND | LOCK_EX);
- $json = json_decode("message.txt", true);
-print_r($json);
-$output = json_decode(file_get_contents('php://input'),true);
-$id = $output['message']['chat']['id'];
-$firstName=$output['message']['from']['first_name'];
-if($output['message']['text']==='checkin'){
-file_get_contents("https://api.telegram.org/bot376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8/sendMessage?chat_id=".$id."&text=hi   ".$firstName."-(dolbaeb)");
-}
-  //file_put_contents("logs.txt",$id);
-
-

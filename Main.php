9138d61e7b48b334590f88ce6441934f2f84f92e
volidamar <volidamar@mail.ru>
2017-03-01 13:22:18 +0200

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,14 +1,1 @@
-<?php 
 
-class Main{
-    
- public $x=array(1,2,3,4);
-    public $y;
-    
-    public function run(){
-      print_r($this->x);  
-    }
-    
-}
-$lol=new Main();
-$lol->run();

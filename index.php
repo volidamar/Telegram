29e14b1778947f1eee759b1d8b55203875849e55
volidamar <volidamar@mail.ru>
2017-03-01 15:36:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,4 +9,5 @@
 $filePath='https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/getupdates';
 
     $newMain = new Main();
-    $newMain->run($filePath);
+    //$newMain->run($filePath);
+print_r($newMain->R[1]->workTime);

5ef6a911a4937bf78b73c84f347f7a7fd2ad3e1d
volidamar <volidamar@mail.ru>
2017-03-01 18:12:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,3 @@ $filePath='https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStD
 
     $newMain = new Main();
     $newMain->run($filePath);
-    $workTime=serialize($newMain->R);
-    file_put_contents("doc/message.txt",$workTime,FILE_APPEND | LOCK_EX);
-    $x=file_get_contents("doc/message.txt");
-    $un=unserialize($x);
-print_r($un);

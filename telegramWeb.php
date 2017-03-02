318572f14a58f77f699c28e65ff9ccd979b42c1d
volidamar <volidamar@mail.ru>
2017-03-02 15:02:15 +0200

    Update telegramWeb.php

--- telegramWeb.php
+++ telegramWeb.php
@@ -10,8 +10,8 @@
 
 $output = file_get_contents('php://input');
 $output=$output.',';
-file_put_contents("message.txt",$output,FILE_APPEND | LOCK_EX);
-$json = json_decode("message.txt", true);
+file_put_contents("doc/message.txt",$output,FILE_APPEND | LOCK_EX);
+$json = json_decode("doc/message.txt", true);
 print_r($json);
 $token='376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8';
 $output = json_decode(file_get_contents('php://input'),true);

d7ff04b7c7f72626faff275460be2fb8a6c637dc
volidamar <volidamar@mail.ru>
2017-03-02 15:16:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ $filePath='https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStD
 
    $newMain = new Main();
     $newMain->run($filePath);
-    $workTime=serialize($newMain->R);
-    file_put_contents("doc/workTime.txt",$workTime,FILE_APPEND | LOCK_EX);
+  
  
     

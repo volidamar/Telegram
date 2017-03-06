f658fcc05f3d11e25b5bd2e714cc06808c5f40fa
volidamar <volidamar@mail.ru>
2017-03-06 15:55:12 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -15,16 +15,16 @@ file_put_contents("message.txt",$output,FILE_APPEND | LOCK_EX);
 $json = json_decode("message.txt", true);
 $token='376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8';
 //$output = json_decode(file_get_contents('php://input'),true);
-$id = $output['message']['chat']['id'];
-$firstName=$output['message']['from']['first_name'];
-$message=$output['message']['text'];
+//$id = $output['message']['chat']['id'];
+//$firstName=$output['message']['from']['first_name'];
+//$message=$output['message']['text'];
 
 //include('../index.php');
 
-$dataTime = $output['message']['date'];
-$datee = date("d-m-Y H:i:s", $dataTime);
-$date = new DateTime($datee);
-$date->format('Y-m-d H:i:s');
+//$dataTime = $output['message']['date'];
+//$datee = date("d-m-Y H:i:s", $dataTime);
+//$date = new DateTime($datee);
+//$date->format('Y-m-d H:i:s');
 
 
 
@@ -51,19 +51,19 @@ $x='your work time for today :' . $time . ":" . $min . ":" . $sec;
 }*/
      
   
-switch($message){
-    case '/start':
-        $message='HELLO';
-        sendMessage($token,$id,$message.KeyboardMenu());
-        break;
-        case 'keyboard':
-             $message='the keyboard is ready';
-        sendMessage($token,$id,$message.KeyboardMenu());
-        break;
-    case 'hi':
-        $message='Hello';
-        sendMessage($token,$id,$message);
-        break;
+//switch($message){
+   // case '/start':
+      //  $message='HELLO';
+      //  sendMessage($token,$id,$message.KeyboardMenu());
+     //   break;
+     //   case 'keyboard':
+     //        $message='the keyboard is ready';
+    //    sendMessage($token,$id,$message.KeyboardMenu());
+   //     break;
+  //  case 'hi':
+      //  $message='Hello';
+      //  sendMessage($token,$id,$message);
+     //   break;
    /* case 'checkout';      
 foreach($newMain->R as $res){
  if($res->messageIdCheckout==$messageId)
@@ -73,28 +73,28 @@ foreach($newMain->R as $res){
            sendMessage($token,$id,$message);
 
         break;*/
-    case 'how are you?':
-        $message='i am fine';
-        sendMessage($token,$id,$message);
-        break;
+   // case 'how are you?':
+      //  $message='i am fine';
+      //  sendMessage($token,$id,$message);
+      //  break;
  
-}
-function sendMessage($token,$id,$message)
-{
- file_get_contents("https://api.telegram.org/bot" .$token. "/sendMessage?chat_id=".$id."&text=".$message);
-}
-function KeyboardMenu()
-{
-         $reply_markup = '';
-    $buttons = [['checkin','checkout'],['pause','resume']];
-    $keyboard = json_encode($keyboard = [
-        'keyboard' => $buttons /*[$buttons]*/,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'parse_mode' => 'HTML',
-        'selective' => true
-    ]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
+//}
+//function sendMessage($token,$id,$message)
+//{
+ //file_get_contents("https://api.telegram.org/bot" .$token. "/sendMessage?chat_id=".$id."&text=".$message);
+//}
+//function KeyboardMenu()
+//{
+      //   $reply_markup = '';
+  //  $buttons = [['checkin','checkout'],['pause','resume']];
+  //  $keyboard = json_encode($keyboard = [
+  //      'keyboard' => $buttons /*[$buttons]*/,
+   //     'resize_keyboard' => true,
+   //     'one_time_keyboard' => false,
+      //  'parse_mode' => 'HTML',
+      //  'selective' => true
+  //  ]);
+ //   $reply_markup = '&reply_markup=' . $keyboard . '';
     
-    return $reply_markup;
-}
+ //   return $reply_markup;
+//}

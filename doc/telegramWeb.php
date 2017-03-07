d94a62f5dda63dfa0cadc389cf56d7b455ee5e93
volidamar <volidamar@mail.ru>
2017-03-07 16:24:42 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -12,14 +12,16 @@ $output = file_get_contents('php://input');
 $messageId=$output['message']['message_id'];
 $output=$output.',';
 file_put_contents("message.txt",$output,FILE_APPEND | LOCK_EX);
+include('../index.php');
 $json = json_decode("message.txt", true);
+
 $token='376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8';
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $firstName=$output['message']['from']['first_name'];
 $message=$output['message']['text'];
 
-include('../index.php');
+
 
 $dataTime = $output['message']['date'];
 $datee = date("d-m-Y H:i:s", $dataTime);

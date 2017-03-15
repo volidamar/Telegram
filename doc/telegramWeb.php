29e1f32fb43763a722927f51692a0069bd5c58ad
volidamar <volidamar@mail.ru>
2017-03-15 14:11:43 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -24,7 +24,7 @@ $dataTime = $output['message']['date'];
 $datee = date("d-m-Y H:i:s", $dataTime);
 $date = new DateTime($datee);
 $date->format('Y-m-d H:i:s');
-
+include('../index.php');
 if($message=='checkout'){
     foreach($newMain->R as $res){
         if($output['message']['message_id']===$res->messageIdCheckout){

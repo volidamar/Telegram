a84d0e71a5688c70dc8adf70c77730870b28a971
volidamar <volidamar@mail.ru>
2017-03-15 13:24:51 +0200

    Update telegramWeb.php

--- doc/telegramWeb.php
+++ doc/telegramWeb.php
@@ -121,7 +121,7 @@ if($message==='users'){
      sendMessage($token,$id,$x);
  }   */
     foreach ($newMain->lastM as $key=>$res){
-        $status= $key.'-'.$res;
+        $status= $key;
         sendMessage($token,$id,$status);
         }
 }

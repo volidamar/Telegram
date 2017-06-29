a7a2358e42fd94741bf14974964ab849a060f0ee
volidamar <volidamar@mail.ru>
2017-06-29 18:15:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
     
- 
-$output = file_get_contents('php://input');
+   require_once("Main.php");
+    require_once("doc/Database.php");
+    require_once ("doc/Database.php");
 
-file_put_contents("message.txt",$output,FILE_APPEND | LOCK_EX);
-$json = json_decode("message.txt", true);
-print_r($json);
+   //$filePath = 'https://api.telegram.org/bot323490435:AAH6qUwCEZ_01FWYJ3mYp585fDUrbtz4tKw/getupdates';
+   // $filePath='https://api.telegram.org/bot376579345:AAGKlvSF4khe_5X86TLlrYZKS_5bqSdRJf8/getupdates';
+$filePath='https://api.telegram.org/bot329259730:AAEZ-xAi795aQ4BzODtIREBmSurZStDJYYc/getupdates';
+
+   $newMain = new Main();
+    $newMain->run();
 
     
